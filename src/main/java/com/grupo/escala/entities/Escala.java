36157277a27f24f39f913b90dfd10419d1f55e02
rayanebarbosapet <rayanebarbosapet@gmail.com>
2024-02-10 21:18:54 -0300

    implementação das classes

--- src/main/java/com/grupo/escala/entities/Escala.java
+++ src/main/java/com/grupo/escala/entities/Escala.java
@@ -2,11 +2,21 @@ package com.grupo.escala.entities;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Escala {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id_Escala;
-    private Grupo id_Grupo;
-    private Pessoa id;
+  //  private Grupo id_Grupo;
+    //private Pessoa id;
     private LocalDateTime dataEscala;
     private Boolean cumpriu;
      
--- src/main/java/com/grupo/escala/entities/Grupo.java
+++ src/main/java/com/grupo/escala/entities/Grupo.java
@@ -1,9 +1,15 @@
 package com.grupo.escala.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Grupo {
 
+     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id_Grupo;
     private String nome;
-    private Pessoa id;
+  //  private Pessoa id;
     //lista de escalas
 }
